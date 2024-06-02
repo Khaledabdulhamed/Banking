66c5fc3614357f8deec134ddaf29e3db05739ff8
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-06-01 22:47:54 -0400

    Adding new features and authentication

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -19,6 +19,8 @@ import { Input } from "@/components/ui/input"
 import CustomInput from '../CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const formSchema = z.object({
     email: z.string().email(),
@@ -26,11 +28,14 @@ const formSchema = z.object({
 
 
 const AuthForm = ({type}: {type: string}) => {
+    const router = useRouter()
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false);
 
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const formSchema = authFormSchema(type)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
           email: "",
           password: "",
@@ -38,12 +43,35 @@ const AuthForm = ({type}: {type: string}) => {
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof authFormSchema>) {
+      const onSubmit = async(data: z.infer<typeof formSchema>) => {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         setIsLoading(true)
-        console.log(values)
-        setIsLoading(false)
+        
+        try {
+          //sign up with Appwrite & create plaid link  token  
+            if(type === 'sign-up'){
+                const newUser = await signUp(data)
+                setUser(newUser)
+            }
+
+            if(type ==='sign-in'){
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password,
+                })
+            if(response) router.push('/')
+
+            
+            }
+
+
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
       }
 
   return (
@@ -73,9 +101,28 @@ const AuthForm = ({type}: {type: string}) => {
             
             <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {type === 'sign-up' && (
+            <>
+            <div className='flex gap-4'>
+             <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your First Name' />
+             <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your Last Name' />
+             </div>
+             <CustomInput control={form.control} name='address1' label="address" placeholder='Enter your Address' />
+             <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+             <div className='flex gap-4'>
+             <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
+             <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 13212' />
+             </div>
+             <div className='flex gap-4'>
+             <CustomInput control={form.control} name='dateOfBirth' label="Date Of Birth" placeholder='YYYY-MM-DD' />
+             <CustomInput control={form.control} name='ssn' label="SSN" placeholder='123-45-6789' />
+            </div>
+            </>
+        )}
         <CustomInput control={form.control} name='email' label="email" placeholder='Enter your Email' />
         <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
-        <Button type="submit" className='form-btn'>
+        <div className='flex flex-col gap-4'>
+        <Button type="submit" className='form-btn' disabled={isLoading}>
             {isLoading ? (
                 <>
                 <Loader2 size={20} className='animate-spin' /> &nbsp;
@@ -83,10 +130,23 @@ const AuthForm = ({type}: {type: string}) => {
                 </>
             ) : type === 'sign-in' ? 'Sign In' : 'Sign Up' }
         </Button>
+        </div>
       </form>
     </Form>
            
         )}
+        
+    <footer className='flex justify-center gap-1'>
+
+
+       <p className='text-14 font-normal text-gray-600'>{type === 'sign-in'
+       ? "Don't have an account"
+        : "Already have an account"
+        }</p> 
+        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+        {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+        </Link>
+        </footer>
     </section>
   )
 }
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+export const signIn = async () => {
+
+    try {
+        // mutation // database // make fetch
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+export const signUp = async (userData: SignUpParams) => {
+
+    try {
+        // mutation // database // make fetch
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
